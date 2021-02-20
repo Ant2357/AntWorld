c700fc64dad74b7c38ef2f3a8ab809c7c1147642
Ant <ryu4979@gmail.com>
2021-02-20 22:48:17 +0900

    feat: 匠の銅像を設置

--- docs/overviewerConfig.js
+++ docs/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1613461476",
+        "cacheTag": "1613828633",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1613459958,
+            "last_rendertime": 1613826435,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
