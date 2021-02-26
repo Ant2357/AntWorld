118cc44ea1107611fab6691fac025b7c138751b1
Ant <ryu4979@gmail.com>
2021-02-27 04:38:58 +0900

    feat: 船を建造

--- docs/overviewerConfig.js
+++ docs/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1614274095",
+        "cacheTag": "1614367715",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1614273494,
+            "last_rendertime": 1614367096,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
