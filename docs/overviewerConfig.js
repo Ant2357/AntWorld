f92c5aa0b92d3e400086dec50de4c549d5fc44db
Ant <ryu4979@gmail.com>
2021-04-02 15:35:22 +0900

    fix: ネザー島と匠本殿の整備

--- docs/overviewerConfig.js
+++ docs/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1617017589",
+        "cacheTag": "1617344727",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1617013061,
+            "last_rendertime": 1617343992,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
