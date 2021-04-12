473c44f783d62b7a5daa5ea318ea99ac29899488
Ant <ryu4979@gmail.com>
2021-04-12 23:42:41 +0900

    fix: 拠点の整備
    * 道路整備
    * 自動収穫装置付き小麦畑の導入

--- docs/overviewerConfig.js
+++ docs/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1617344727",
+        "cacheTag": "1618237544",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1617343992,
+            "last_rendertime": 1618237041,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
