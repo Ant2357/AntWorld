358c46235015f02f291ca91559a7bbadad18dc61
Ant <ryu4979@gmail.com>
2021-04-15 22:15:12 +0900

    fix: 自動収穫装置付き小麦畑の改良

--- docs/overviewerConfig.js
+++ docs/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1618237544",
+        "cacheTag": "1618492079",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1618237041,
+            "last_rendertime": 1618491573,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
