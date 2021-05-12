45245eeecd30d6c401a5ee15bcf6c24d3cae6dbc
Ant <ryu4979@gmail.com>
2021-05-13 00:32:47 +0900

    feat: 蟻拠点と灯台を結ぶ道路の作成

--- docs/overviewerConfig.js
+++ docs/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1618492079",
+        "cacheTag": "1620832981",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1618491573,
+            "last_rendertime": 1620832037,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
